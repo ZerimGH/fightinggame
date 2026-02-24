d17bcfbb513b32525d8bb1304d13a11f589a60a1
lewis <lewdon0001@gmail.com>
2026-02-24 05:28:39 +0000

    Add info to shared.h

--- src/net/shared.h
+++ src/net/shared.h
@@ -1,8 +1,7 @@
-#define MAX_PLAYERS 2
-#define FRAMERATE 60 
-#define POLL_MS ((1000 / (FRAMERATE)) / 2)
-// #define POLL_MS 8 
-#define CONNECTION_WAIT_MS 10000
-#define HOST ENET_HOST_ANY
-#define PORT 58008
-#define MAX_ROLLBACK 8 
+#define MAX_PLAYERS 2 // The number of players that can join a game
+#define FRAMERATE 60 // The framerate the game will run at 
+#define POLL_MS ((1000 / (FRAMERATE))) // The time allowed for polling the server
+#define CONNECTION_WAIT_MS 10000 // The time allowed waiting for a connection response
+#define HOST ENET_HOST_ANY // Server host
+#define PORT 58008 // Server port
+#define MAX_ROLLBACK 8 // The number of inputs history to store
