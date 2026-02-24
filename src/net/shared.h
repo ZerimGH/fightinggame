ba348f7abae68c4ae5b22b6de9e836d2819d755f
lewis <lewdon0001@gmail.com>
2026-02-24 05:30:57 +0000

    Fix poll time

--- src/net/shared.h
+++ src/net/shared.h
@@ -1,6 +1,6 @@
 #define MAX_PLAYERS 2 // The number of players that can join a game
 #define FRAMERATE 60 // The framerate the game will run at 
-#define POLL_MS ((1000 / (FRAMERATE))) // The time allowed for polling the server
+#define POLL_MS ((1000 / (FRAMERATE)) / 2) // The time allowed for polling the server
 #define CONNECTION_WAIT_MS 10000 // The time allowed waiting for a connection response
 #define HOST ENET_HOST_ANY // Server host
 #define PORT 58008 // Server port
